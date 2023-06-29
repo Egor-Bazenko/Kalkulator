f818b62fe87208adadd59b6766dfda8972f749e9
Egor-Bazenko <129248412+Egor-Bazenko@users.noreply.github.com>
2023-06-29 22:22:40 +1000

    Add files via upload

---
+++ WindowsFormsApplication19/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WindowsFormsApplication19")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WindowsFormsApplication19")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+/* Установка для ComVisible значения false делает типы в этой сборке невидимыми
+ в COM-компоненты. Если вам нужно получить доступ к типу в этой сборке из
+ COM, установите для атрибута ComVisible значение true для этого типа.*/
+[assembly: ComVisible(false)]
+[assembly: Guid("03b4fbf0-aba3-407b-9e16-c7e516ec1d2a")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
